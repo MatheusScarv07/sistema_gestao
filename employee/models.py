a9e0b89fd4603742ff6a25285615d201bbeb8d50
MatheusCarvalho <107969414+MatheusScarv07@users.noreply.github.com>
2024-09-08 15:57:06 -0300

    Update models.py

--- employee/models.py
+++ employee/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    nome = models.TextField(max_length=11, null=False)
+    nome = models.TextField(max_length=70, null=False)
     cargo = models.TextField(null=True, blank=True)
     status = models.TextField(null=True, blank=True)
 
