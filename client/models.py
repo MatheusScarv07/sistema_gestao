e3ad53a8851b4ad39a0c5bce7a35814620d3af02
Cristian Viery <viery.guimaraes01@gmail.com>
2024-09-08 16:54:22 -0300

    arrumado banco

--- client/models.py
+++ client/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Client(models.Model):
-    nome = models.CharField(max_length=70, null=False )
+    nome = models.CharField(max_length=70, null=True )
     email = models.EmailField(unique=True, null=True, blank=True)
-    telefone = models.CharField(max_length=11, null=False )
-    cpf_cnpj_cliente = models.CharField(max_length=14, null=False, unique=True)
+    telefone = models.CharField(max_length=11, null=True )
+    cpf_cnpj_cliente = models.CharField(max_length=14, null=True, unique=True)
     rg_cliente = models.CharField(max_length=15, null=True)
-    inscricao_estadual_cliente = models.CharField(max_length=9, null=False)
+    inscricao_estadual_cliente = models.CharField(max_length=9, null=True)
     numero_casa = models.IntegerField(null=True)
     complemento = models.CharField(max_length=100, null=True)
     bairro = models.CharField(max_length=70, null=True)
     cidade = models.CharField(max_length=50, null=True)
     estado = models.CharField(max_length=2, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    models.DateTimeField(default=timezone.now)
--- supplier/models.py
+++ supplier/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Supplier(models.Model):
-    nome = models.CharField(max_length=70, null=False)
-    contato = models.CharField(max_length=11, null=False)
-    endereco = models.CharField(max_length=255, null=False)
-    cnpj = models.CharField(max_length=14, null=False)
+    nome = models.CharField(max_length=70, null=True)
+    contato = models.CharField(max_length=11, null=True)
+    endereco = models.CharField(max_length=255, null=True)
+    cnpj = models.CharField(max_length=14, null=True)
     email = models.EmailField(null=True, blank=True)
