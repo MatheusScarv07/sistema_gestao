7eff2e0e7bbc8b3ef3894c75f80decb6143a8f96
Cristian Viery <viery.guimaraes01@gmail.com>
2024-10-25 16:31:26 -0300

    modelo

---
+++ budget/static/budget/js/new_budget.js
@@ -0,0 +1,35 @@
+
+function price_finally() {
+    // Pegando os valores de quantidade e preço unitário
+    const quantity = document.getElementById('quantity').value;
+    const unitPrice = document.getElementById('unit_price').value;
+    
+    // Convertendo os valores para números e calculando o total
+    const totalPrice = parseFloat(quantity) * parseFloat(unitPrice);
+    
+    // Exibindo o preço total, formatando para duas casas decimais
+    if (!isNaN(totalPrice)) {
+        document.getElementById('total_price').value = totalPrice.toFixed(2);
+    } else {
+        document.getElementById('total_price').value = '';
+    }
+}
+
+
+function fetchProductData(productId) {
+    // Faz uma requisição fetch para obter os dados do produto
+    fetch(`/sales/get_product/${productId}`)
+        .then(response => response.json()) // Converte a resposta para JSON
+        .then(data => {
+            console.log(data); // Exibe os dados no console
+            if (data.error) {
+                // Mostra um alerta se houver um erro nos dados recebidos
+                alert(data.error);
+            } else {
+                // Preenche os campos do formulário com os dados do produto
+                document.getElementById(`product_name`).value = data.name;
+                document.getElementById(`unit_price`).value = data.price;
+            }
+        })
+        .catch(error => console.error('Error:', error)); // Tratamento de erros
+  }
