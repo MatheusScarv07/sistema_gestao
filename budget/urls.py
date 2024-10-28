2772c67e1da621b1b7c3c370bd640a1248f4dfbe
MatheusCarvalho <107969414+MatheusScarv07@users.noreply.github.com>
2024-10-28 20:27:24 -0300

    ajustar_relatorio

--- budget/urls.py
+++ budget/urls.py
@@ -1,13 +1,15 @@
-from budget.views import home,budget_search, search_budget_filter,new_budget, enviar_orcamento, cart, excluir_produto
+from budget.views import home,budget_search, search_budget_filter,new_budget, enviar_orcamento, cart, excluir_produto , gerar_relatorio , enviar_whatsapp
 from django.urls import path
 
 
 urlpatterns = [
-    path('budget/',home ),
-    path('budget/search_budget/',budget_search),
+    path('budget/', home),
+    path('budget/search_budget/', budget_search),
     path('budget/new_budget/', new_budget),
-    path('budget/enviar_orcamento/',enviar_orcamento),
-    path('budget/filtrar_budget/',search_budget_filter ),
+    path('budget/enviar_orcamento/', enviar_orcamento),
+    path('budget/filtrar_budget/', search_budget_filter),
     path('budget/salvar-carrinho/', cart),
-    path('budget/excluir_produto/<int:id>', excluir_produto),
+    path('budget/excluir_produto/<int:id>/', excluir_produto),  # Nota: "/" no final
+    path('budget/gerar-relatorio/', gerar_relatorio, name='gerar_relatorio'),  # Nota: caminho correto
+    path('budget/enviar_whatsapp/', enviar_whatsapp, name='enviar_whatsapp'),  # Nota: "/" no final
 ]
