19f466caf26998b99beada0490083269c6fffb41
Cristian Viery <viery.guimaraes01@gmail.com>
2024-11-05 18:13:58 -0300

    alguns erros

--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from payment.views import home, search_payment,new_payment
+from payment.views import home, search_payment,new_payment,new_payment_nfe
 
 urlpatterns = [
     path('payment/', home, name='home_payment'),  # Adiciona um nome à URL
     path('new_payment/', new_payment, name='new_payment'),
-    path('search_payment/', search_payment, name='search_payment'),  # Adiciona um nome à URL
+    path('search_payment/', search_payment, name='search_payment'),
+    path('new_payment/<int:num_nota>/', new_payment_nfe, name='new_payment_nfe'),
 ]
