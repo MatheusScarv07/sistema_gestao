310728103305765bcc619ec696847f347a948d55
Cristian Viery <viery.guimaraes01@gmail.com>
2024-12-18 13:19:49 -0300

    bug login

--- login/views.py
+++ login/views.py
@@ -10,20 +10,25 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+
+            # Testando autenticação
+            if request.user.is_authenticated:
+                print(f"Usuário autenticado: {request.user.username}")
+            else:
+                print("Usuário não autenticado após login.")
             
-            # Verificando o parâmetro 'next' para redirecionar para a página correta
-            next_url = request.GET.get('next', 'inicio')  # Se não houver 'next', usa 'inicio' como padrão
-            print(f"Redirecionando para: {next_url}")  # Imprime o valor de next_url para depuração
+            # Redirecionar para a página definida no parâmetro 'next' ou 'inicio'
+            next_url = request.POST.get('next', 'inicio')
+            print(f"Redirecionando para: {next_url}")
             return redirect(next_url)
         else:
-            print(form.errors)  # Adiciona um log de erro para ajudar a depurar
+            print(form.errors)
             messages.error(request, 'Credenciais inválidas. Tente novamente.')
     else:
         form = AuthenticationForm()
 
-    return render(request, 'login/pages/login.html', {'form': form})
+    return render(request, 'login/pages/login.html', {'form': form, 'next': request.GET.get('next', '/')})
 
 def logout_view(request):
     logout(request)
-    # Redirecionar para a página de login após o logout
     return redirect('login')
